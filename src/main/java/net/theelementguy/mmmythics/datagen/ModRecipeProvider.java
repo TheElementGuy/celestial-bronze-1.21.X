c91775514dc25b3a3e3490bf9f95f9eb647d9659
Jude Rasmussen <jude.a.rasmussen@gmail.com>
2025-07-19 11:38:25 -0700

    Support for new versions of More Metals

--- src/main/java/net/theelementguy/mmmythics/datagen/ModRecipeProvider.java
+++ src/main/java/net/theelementguy/mmmythics/datagen/ModRecipeProvider.java
@@ -6,9 +6,11 @@ import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.data.recipes.RecipeProvider;
 import net.minecraft.data.recipes.*;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.*;
 import net.neoforged.neoforge.common.conditions.IConditionBuilder;
+import net.theelementguy.mmmythics.CelestialBronzeMod;
 import net.theelementguy.mmmythics.item.ModItems;
 
 import java.util.concurrent.CompletableFuture;
@@ -25,9 +27,9 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
     @Override
     protected void buildRecipes(RecipeOutput output) {
 
-        SmithingTransformRecipeBuilder.smithing(Ingredient.of(ModItems.CELESTIAL_BRONZE_UPGRADE_SMITHING_TEMPLATE), Ingredient.of(net.theelementguy.moremetals.item.ModItems.BRONZE_SWORD), Ingredient.of(Items.NETHER_STAR), RecipeCategory.COMBAT, ModItems.CELESTIAL_BRRONZE_SWORD.get()).unlocks("has_ns", has(Items.NETHER_STAR)).save(output, "celestial_bronze_sword_smithing");
+        SmithingTransformRecipeBuilder.smithing(Ingredient.of(ModItems.CELESTIAL_BRONZE_UPGRADE_SMITHING_TEMPLATE), Ingredient.of(net.theelementguy.tegmoremetals.item.ModItems.BRONZE_SWORD), Ingredient.of(Items.NETHER_STAR), RecipeCategory.COMBAT, ModItems.CELESTIAL_BRRONZE_SWORD.get()).unlocks("has_ns", has(Items.NETHER_STAR)).save(output, ResourceLocation.fromNamespaceAndPath(CelestialBronzeMod.MOD_ID, "celestial_bronze_sword_smithing"));
 
-        SmithingTransformRecipeBuilder.smithing(Ingredient.of(ModItems.IMPERIAL_GOLD_UPGRADE_SMITHING_TEMPLATE), Ingredient.of(Items.GOLDEN_SWORD), Ingredient.of(Items.NETHER_STAR), RecipeCategory.COMBAT, ModItems.IMPERIAL_GOLD_SWORD.get()).unlocks("has_ns", has(Items.NETHER_STAR)).save(output, "imperial_gold_sword_smithing");
+        SmithingTransformRecipeBuilder.smithing(Ingredient.of(ModItems.IMPERIAL_GOLD_UPGRADE_SMITHING_TEMPLATE), Ingredient.of(Items.GOLDEN_SWORD), Ingredient.of(Items.NETHER_STAR), RecipeCategory.COMBAT, ModItems.IMPERIAL_GOLD_SWORD.get()).unlocks("has_ns", has(Items.NETHER_STAR)).save(output, ResourceLocation.fromNamespaceAndPath(CelestialBronzeMod.MOD_ID, "imperial_gold_sword_smithing"));
 
     }
 
